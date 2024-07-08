3dee0ea7e2e34a5a96022ff077c6cf2d83ba138d
Victor Dupuy <vdcd120491@gmail.com>
2024-07-08 18:25:40 +0200

    make thenReject bun compliant for convenience

--- src/__tests__/behaviours/thenReject.spec.ts
+++ src/__tests__/behaviours/thenReject.spec.ts
@@ -1,18 +1,35 @@
 import { Mock, when } from "../..";
 
 describe("thenReject", () => {
-  it("should reject any value provided", () => {
+  it("should reject any value provided", async () => {
     const mock = Mock(async () => {});
     when(mock).isCalled.thenReject(new Error("yo"));
-    expect(() => mock()).rejects.toThrow("yo");
+    expect.assertions(1);
+
+    // I'm using bun but it's not iso with jest => no expect().rejects :'(
+    try {
+      await mock();
+    } catch (e) {
+      expect(e).toEqual(new Error("yo"));
+    }
   });
 
-  it("should combine default and custom behaviours", () => {
+  it("should combine default and custom behaviours", async () => {
     const mock = Mock(async (...args: any[]) => {});
     when(mock).isCalled.thenReject(new Error("yo"));
     when(mock).isCalledWith(2).thenReject(new Error("Victor"));
 
-    expect(() => mock()).rejects.toThrow("yo");
-    expect(() => mock(2)).rejects.toThrow("Victor");
+    expect.assertions(2);
+    try {
+      await mock();
+    } catch (e) {
+      expect(e).toEqual(new Error("yo"));
+    }
+
+    try {
+      await mock(2);
+    } catch (e) {
+      expect(e).toEqual(new Error("Victor"));
+    }
   });
 });
